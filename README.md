1d48d05e3a40bf164d94586a23818ebcfed80f87
Erik Ušaj <erikusaj@hotmail.com>
2016-05-21 17:47:58 +0200

    new file:   checkOpenCV.js

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # checkOpenCV
-Script to check OpenCV version
+Script for checking OS, Node, Npm and **OpenCV** versions.
 
-Node script for checking OS version, Node, Npm and OpenCV versions.
+## Running script
 
+```bash
+$ node checkOpenCV.js
+```
+
+## Note
 I am observing issues installing Npm module _opencv_ Node Bindings to OpenCV from [npm opencv ](https://www.npmjs.com/package/opencv) on several Vagrant boxes and Docker containers. Therefore creating s script to try identify the issue.
---
+++ checkOpenCV.js
@@ -0,0 +1,78 @@
+//
+// to run script type:
+// node checkOpenCV.js
+//
+
+"use strict";
+var os = require('os');
+var exec = require("child_process").exec;
+var fs = require("fs");
+
+console.log( [ os.type() , os.platform() ,os.arch() , os.release(), os.totalmem(), os.freemem()
+].join(' ; ') );
+
+exec('node --version', (error, stdout, stderr) => {
+  if (error) {
+    console.error(`Node: ERROR: ${error}`);
+    return;
+  }
+  else {
+    console.log( 'Node: '.concat( stdout.split('\n')[0] ));
+  }
+});
+
+exec('npm -version', (error, stdout, stderr) => {
+  if (error) {
+    console.error(`Npm: ERROR: ${error}`);
+    return;
+  }
+  else {
+    var npmversion = stdout.split('\n')[0];
+    // console.log( 'Npm: '.concat( npmversion ));
+    exec('npm config get prefix', (error, stdout, stderr) => {
+      if (error) {
+        console.error(`Npm: ERROR: ${error}`);
+        return;
+      }
+      else {
+        console.log( 'Npm:' + npmversion + ' ; ' + stdout.split('\n')[0] );
+      }
+    });
+
+  }
+});
+
+if ( os.platform() == 'win32' )
+{
+  console.error('TODO: Improvement: Checking OpenCV on Windows.');
+}
+else {
+  exec('sudo find / -name "opencv.pc" -type f', (error, stdout, stderr) => {
+    if (error) {
+      console.error(`exec error: ${error}`);
+      return;
+    }
+    else {
+      var found = stdout.split('\n');
+      for (var i in found)
+      {
+        if (found[i] != "")
+        {
+          var filename = found[i];
+          fs.readFile(filename, function (err, data) {
+          if (err) {
+             return console.error(err);
+          }
+            var lines = data.toString().split('\n');
+            for (var l in lines)
+            {
+              var ls = lines[l].split(':');
+              if ( ls[0].trim().toLowerCase() == 'version' )
+              console.log( ['OpenCV',  ls[1].trim(), filename].join(' ; ') );
+            }
+          });
+        }
+      }
+    }
+  });
+}
